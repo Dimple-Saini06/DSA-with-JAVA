03a3736255b69f1224ca5c27aa006a930a8ab653
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-12-22 16:29:22 +0530

    Day-4

--- Day-3/Ques.java
+++ Day-3/Ques.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Ques{
+    //function
     /*public static int product(int a, int b){
         return a*b;
     }*/
@@ -12,7 +13,8 @@ public class Ques{
         return fact;
     }*/
 
-    public static int overloading(int a, int b){
+    //overloading
+    /*public static int overloading(int a, int b){
         return a+b;
     }
 
@@ -22,8 +24,100 @@ public class Ques{
 
     public static int overloading(char a, char b){
         return a+b;
+    }*/
+
+
+   //prime nums
+    /*public static boolean isPrime(int n){
+        if(n==2){
+            return true;
+        }
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }*/
+
+
+   //Binary To Decimal
+    /*public static int binTodec(int num){
+        int pow=0, dec=0;
+        while(num>0){
+            int ld = num%10;
+            dec = dec + ld * ((int)Math.pow(2,pow));
+            pow++;
+            num /=10;
+        }
+
+        return dec;
+    }*/
+
+    //Decimal To Binary
+    /*public static int decTobin(int num){
+        int pow=0, bin=0;
+        while(num>0){
+            int ld = num%2;
+            bin = bin+ld * ((int) Math.pow(10, pow));
+            pow++;
+            num /= 2;
+        }
+        return bin;
+    }*/
+
+    /*public static boolean evenOrodd(int n){
+       if(n%2 == 0){
+        return true;
+       } 
+       else {
+        return false;
+       }
     }
 
+    public static void output(int n){
+        if(evenOrodd(n)){
+            System.out.print("The number is even.");
+        }
+        else{
+            System.out.print("The number is odd.");
+        }
+    }*/
+    
+    /*public static boolean palidrome(int n){
+        int pow = 0, val=0;
+        int dup = n;
+        while(dup>0){
+            int ld = dup%10;
+            val = val*10+ld;
+            pow++; dup /= 10;
+        }
+        if(n==val){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }*/
+
+    public static int sumOfdigits(int num){
+        int sum = 0;
+        while(num>0){
+            int ld = num%10;
+            sum += ld;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+    public static int getInput(){
+        System.out.print("Enter a Number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        return sumOfdigits(n);
+    }
+    
     public static void main(String args[]){
         /*System.out.print("Enter a Number: ");
         Scanner sc = new Scanner(System.in);
@@ -78,10 +172,16 @@ public class Ques{
         /*System.out.println(product(5,6));
         System.out.println(factorial(5));*/
 
-        System.out.println(overloading(13f,6f));
+        /*System.out.println(overloading(13f,6f));
         System.out.println(overloading(13,6));
-        System.out.println(overloading('a','b'));
-        
+        System.out.println(overloading('a','b'));*/
+
+        // System.out.print(isPrime(613));
+        // System.out.println(binTodec(1101));
+        // System.out.print(decTobin(13));
+        // output(72);
+
+        System.out.print(getInput());
     }
 }
 
---
+++ Day-4/Ques.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Ques{
+    /*public static int[] update(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            arr[i] = arr[i]+1;
+        }
+       return arr;
+    }*/
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5,6};
+        /*System.out.println(arr[5]);
+        arr[5] = 10;
+        System.out.println(arr[5]);*/
+
+        /*update(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }*/
+        
+    }
+}
