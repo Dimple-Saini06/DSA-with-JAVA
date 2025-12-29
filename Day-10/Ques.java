12a0b43e6e0f367d32ba598c8c0cac355b5eb729
Dimple-Saini06 <dimple13619saini@gmail.com>
2025-12-29 19:05:38 +0530

    Day-10

--- Day-10/Ques.java
+++ Day-10/Ques.java
@@ -31,7 +31,7 @@ public class Ques{
         return false;
     }*/
 
-    public static int[][] transposedMatrix(int matrix[][]){
+    /*public static int[][] transposedMatrix(int matrix[][]){
         int transMatrix[][] = new int[matrix[0].length][matrix.length];
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
@@ -39,6 +39,17 @@ public class Ques{
             }
         }
         return transMatrix;
+    }*/
+
+    //QUES:4
+    public static boolean isPalidrome(String s){
+        int n = s.length();
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i) != s.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
     }
     public static void main(String args[]){
         //QUES:1
@@ -57,13 +68,17 @@ public class Ques{
         System.out.print(searchInmatrix(matrix, 1));*/
 
         //QUES:3
-        int matrix[][] = {{1,2,3},{4,5,6}};
+        /*int matrix[][] = {{1,2,3},{4,5,6}};
         int transMatrix[][] = transposedMatrix(matrix);
         for(int i=0;i<transMatrix.length;i++){
             for(int j=0;j<transMatrix[0].length;j++){
                 System.out.print(transMatrix[i][j] + " ");
             }
             System.out.println();
-        }
+        }*/
+
+        //QUES:4
+        String s = "raar";
+        System.out.print(isPalidrome(s));
     }
 }
