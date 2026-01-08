bb3983f1e78420610997a457c386eed561a29d1a
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-01-08 20:43:32 +0530

    PROBLEMS

--- Leetcode-problems/Ques.java
+++ Leetcode-problems/Ques.java
@@ -1,6 +1,6 @@
 public class Ques{
     public static void main(String args[]){
-        int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        /*int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
 
         int n = matrix.length, m = matrix[0].length;
         for(int i=0;i<n;i++){
@@ -14,12 +14,36 @@ public class Ques{
             System.out.println();
             
         }
-System.out.println("matrix print.......");
+        System.out.println("matrix print.......");
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 System.out.print(matrix[i][j]);
             }
             System.out.println();
+        }*/
+
+        int nums[] = {1,1,2};
+        int c = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i+1 < nums.length && nums[i] != nums[i+1]){
+                c++;
+            }
+        }
+        if(nums[nums.length-1] != nums[nums.length-2]){
+            c++;
+        }
+        int count[] = new int[c]; int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(i+1 < nums.length && nums[i] != nums[i+1]){
+                count[k++] = nums[i]; 
+            }
+        }
+        if(nums[nums.length-1] != nums[nums.length-2]){
+            count[count.length-1] = nums[nums.length-1]; 
+        }
+        System.out.println(c);
+        for(int i=0;i<count.length;i++){
+            System.out.print(count[i]);
         }
     }
 }
